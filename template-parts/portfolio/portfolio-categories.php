9ca2bea2c171ffdc7ac800752ceb1c2e53cafad2
Robert Schmahl <69170803+rschm007@users.noreply.github.com>
2023-05-25 16:08:44 -0700

    portfolio page mobile styles

--- template-parts/portfolio/portfolio-categories.php
+++ template-parts/portfolio/portfolio-categories.php
@@ -9,7 +9,7 @@
  */
 ?>
 
-<div class="w-full grid grid-cols-3 gap-x-5 px-12 mt-6">
+<div class="w-full flex flex-col items-center justify-center space-y-8 md:space-y-0 md:grid md:grid-cols-3 md:gap-x-5 px-12 md:mt-6 my-12">
     <?php if (have_rows('portfolio_category_one')) : ?>
         <?php while (have_rows('portfolio_category_one')) : the_row();
 
@@ -20,10 +20,10 @@
 
         ?>
 
-            <a href="<?php $link; ?>" class="relative flex flex-col items-center justify-center col-span-1 h-2/3 w-full z-10 group">
+            <a href="<?php $link; ?>" class="relative flex flex-col items-center justify-center md:col-span-1 h-56 md:h-2/3 w-full z-10 group">
                 <img class="object-cover bg-cover bg-center w-full h-full group-hover:opacity-40 opacity-100 transition-all duration-150" src="<?php echo esc_url($image['url']); ?>" alt="<?php esc_attr($image['alt']); ?>" />
 
-                <h4 class="absolute text-4xl font-bold -mt-12 bg-slate-800/70 px-8 py-4 z-50">
+                <h4 class="absolute text-3xl md:text-4xl font-bold -md:-mt-12 bg-slate-800/70 px-8 py-4 z-50">
                     <?php the_sub_field('category'); ?>
                 </h4>
             </a>
@@ -41,10 +41,10 @@
 
         ?>
 
-            <a href="<?php $link; ?>" class="relative flex flex-col items-center justify-center col-span-1 h-2/3 w-full z-10 group">
+            <a href="<?php $link; ?>" class="relative flex flex-col items-center justify-center md:col-span-1 h-56 md:h-2/3 w-full z-10 group">
                 <img class="object-cover bg-cover bg-center w-full h-full group-hover:opacity-40 opacity-100 transition-all duration-150" src="<?php echo esc_url($image['url']); ?>" alt="<?php esc_attr($image['alt']); ?>" />
 
-                <h4 class="absolute text-4xl font-bold -mt-12 bg-slate-800/70 px-8 py-4 z-50">
+                <h4 class="absolute text-3xl md:text-4xl font-bold -md:-mt-12 bg-slate-800/70 px-8 py-4 z-50">
                     <?php the_sub_field('category'); ?>
                 </h4>
             </a>
@@ -62,10 +62,10 @@
 
         ?>
 
-            <a href="<?php $link; ?>" class="relative flex flex-col items-center justify-center col-span-1 h-2/3 w-full z-10 group">
+            <a href="<?php $link; ?>" class="relative flex flex-col items-center justify-center md:col-span-1 h-56 md:h-2/3 w-full z-10 group">
                 <img class="object-cover bg-cover bg-center w-full h-full group-hover:opacity-40 opacity-100 transition-all duration-150" src="<?php echo esc_url($image['url']); ?>" alt="<?php esc_attr($image['alt']); ?>" />
 
-                <h4 class="absolute text-4xl font-bold -mt-12 bg-slate-800/70 px-8 py-4 z-50">
+                <h4 class="absolute text-3xl md:text-4xl font-bold -md:-mt-12 bg-slate-800/70 px-8 py-4 z-50">
                     <?php the_sub_field('category'); ?>
                 </h4>
             </a>
