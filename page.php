73bad39bc6a5bb6918002c82f19bb14c4a156150
Robert Schmahl <69170803+rschm007@users.noreply.github.com>
2023-06-29 18:40:38 -0700

    current page search function

--- page.php
+++ page.php
@@ -26,9 +26,46 @@ get_header();
 			<?php single_post_title() ?>
 		</h3>
 
-		<div class="posts-search flex flex-row justify-center items-center w-full my-12">
-			<?php get_search_form(); ?>
-		</div>
+		<!-- search script - searches current page posts and removes non-matching items -->
+		<script>
+			$(document).ready(function() {
+				$("#search-submit").click(function(e) {
+					e.preventDefault();
+
+					const value = $("#default-search").val().trim().toLowerCase();
+
+					// find all elements that don't have a matching ID
+					const elements = [...document.querySelectorAll(`.post-block`)];
+					const matchingIds = elements.filter((el) => {
+						return !el.id.toLowerCase().includes(value);
+					});
+
+					// set matching elements to hidden
+					matchingIds.forEach((el) => {
+						el.classList.add("hidden");
+					});
+				});
+			});
+		</script>
+
+		<form id="posts-search" class="flex flex-row justify-center items-center w-full my-12">
+			<label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+
+			<div class="relative w-[30rem]">
+
+				<div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+					<svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+					</svg>
+				</div>
+
+				<input type="search" id="default-search" name="search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-[#e4c28b] focus:border-[#e4c28b]" placeholder="Search <?php single_post_title() ?> posts..." required>
+
+				<button id="search-submit" type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-black hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-[#e4c28b] font-medium rounded-lg text-sm px-4 py-2 transition-all duration-150 ease-in-out">
+					Search
+				</button>
+			</div>
+		</form>
 
 		<div class="w-full h-full flex flex-row items-center justify-center flex-wrap gap-8 my-16">
 			<?php foreach ($subpage_category as $post) :
@@ -37,7 +74,7 @@ get_header();
 
 				<?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail'); ?>
 
-				<a href="<?php the_permalink(); ?>" class="relative flex flex-col items-center justify-center md:col-span-1 h-96 w-96 z-10 group">
+				<a id="<?php the_title(); ?>" href="<?php the_permalink(); ?>" class="post-block relative flex flex-col items-center justify-center md:col-span-1 h-96 w-96 z-10 group">
 					<img class="object-cover bg-cover bg-center w-full h-full group-hover:opacity-40 opacity-100 transition-all duration-150" src="<?php echo $image[0]; ?>" alt="<?php the_title(); ?>" />
 
 					<h4 class="text-2xl font-light -md:-mt-12 bg-slate-900/90 w-full px-8 py-4 z-50 text-center">
